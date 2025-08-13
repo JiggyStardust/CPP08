86ef32f584ee8756159981c289b1de274dc29928
JiggyStardust <saaraleni@gmail.com>
2025-08-13 13:59:12 +0300

    Iterator added, almost all tests ready

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -2,3 +2,34 @@
 
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <stack>
+
+
+template <class T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack() = default;
+		~MutantStack() = default;
+		MutantStack(const MutantStack& orig) = default;
+		MutantStack& operator = (const MutantStack& orig) = default;
+
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		iterator begin();
+		iterator end();
+		const_iterator cbegin() const;
+		const_iterator cend() const;
+		reverse_iterator rbegin();
+		const_reverse_iterator crbegin() const;
+		reverse_iterator rend();
+		const_reverse_iterator crend() const;
+
+};
+
+#include "MutantStack.tpp"
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,7 +1,78 @@
 #include "../inc/MutantStack.hpp"
 
+// int main()
+// {
+// 	std::cout << "TEST 1." << std::endl;
+	
+// 	return 0;
+// }
+
 int main()
 {
-	std::cout << "TEST 1." << std::endl;
+	MutantStack<int> mstack;
+
+	std::cout << "The stack is empty: ";
+	if (mstack.empty())
+		std::cout << "true\n";
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "The stack is empty: ";
+	if (mstack.empty())
+		std::cout << "true\n";
+	else
+		std::cout << "false\n";
+	std::cout << "The size is: ";
+	std::cout << mstack.size() << std::endl;
+	std::cout << "On top we have: ";
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+
+	std::cout << "After popping the size is: ";
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	std::cout << "After 4 additional pushes the size is: ";
+	std::cout << mstack.size() << std::endl;
+	
+	
+	std::cout << "iterator:\n";
+	MutantStack<int>::iterator it = mstack.begin();
+	++it;
+	--it;
+	while (it != mstack.end())
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	MutantStack<int> s(mstack);
+	std::cout << "\nconst_iterator for copy:\n";
+	MutantStack<int>::const_iterator ite = s.begin();
+	while (ite != s.end())
+	{
+		std::cout << *ite << std::endl;
+		++ite;
+	}
+
+	MutantStack<int> r(mstack);
+	std::cout << "\nreverse_iterator:\n";
+	MutantStack<int>::reverse_iterator rit = r.rbegin();
+	MutantStack<int>::reverse_iterator rend = r.rend();
+	while (rit != rend)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	std::cout << "\nconst_reverse_iterator:\n";
+	MutantStack<int>::const_reverse_iterator crit = r.crbegin();
+	MutantStack<int>::const_reverse_iterator crend = r.crend();
+	while (crit != crend)
+	{
+		std::cout << *crit << std::endl;
+		++crit;
+	}
+
 	return 0;
 }
